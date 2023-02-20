372178ddf7fad76f2ece60be5e2d6e2633d5f4cf
Edward <73746306+WangEdward@users.noreply.github.com>
2023-02-21 00:35:25 +0800

    chore: update result wording

--- src/components/ResultCard.vue
+++ src/components/ResultCard.vue
@@ -38,7 +38,7 @@ export default {
     <p v-else-if="currentScore <= 59">Minimal to moderate anxiety</p>
     <p v-else-if="currentScore <= 74">Marked to severe anxiety</p>
     <p v-if="currentScore > 74">Most extreme anxiety</p>
-    <p v-if="currentScore > 59">We suggest that you need to visit a psychologist.</p>
+    <p v-if="currentScore > 59">We suggest you to visit a theripist.</p>
   </div>
 </template>
 <style>
