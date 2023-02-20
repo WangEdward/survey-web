1eeb27dab35c03af99f99272390f89bb85525af3
Edward <73746306+WangEdward@users.noreply.github.com>
2023-02-20 21:38:30 +0800

    chore: remove unwanted style

--- src/App.vue
+++ src/App.vue
@@ -89,15 +89,5 @@ header {
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
